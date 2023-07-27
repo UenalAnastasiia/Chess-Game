2a20dbc0c15cf7a7f867051f47de7f6540eaa7c2
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-07-27 13:13:50 +0200

    add chess board and cells

--- src/models/figures/Board.ts
+++ src/models/figures/Board.ts
@@ -1,7 +1,22 @@
-import React from 'react';
+import { Cell } from "./Cell";
+import { Colors } from "./Colors";
 
-const Board = () => {
-    
-};
+export class Board {
+    cells: Cell[][] = [];
 
-export default Board;
+
+    public initCells() {
+        for (let i = 0; i < 8; i++) {
+            const row: Cell[] = [];
+            for (let j = 0; j < 8; j++) {
+                if ((i + j) % 2 !== 0) {
+                    row.push(new Cell(this, j, i, Colors.BLACK, null));       // Schwarze Felder
+                } else {
+                    row.push(new Cell(this, j, i, Colors.WHITE, null));       // Weiße Felder
+                }
+            }
+
+            this.cells.push(row);
+        }
+    }
+}
--- src/models/figures/Cell.ts
+++ src/models/figures/Cell.ts
@@ -1,7 +1,24 @@
-import React from 'react';
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Figure } from "./Figure";
 
-const Cell = () => {
+export class Cell {
+    readonly x: number;
+    readonly y: number;
+    readonly color: Colors;
+    figure: Figure | null;
+    board: Board;
+    available: boolean;     // Profe if you can move the figure
+    id: number;     // for react keys
 
-};
 
-export default Cell;
+    constructor(board: Board, x: number, y: number, color: Colors, figure: Figure | null) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.figure = figure;
+        this.board = board;
+        this.available = false;
+        this.id = Math.random();
+    }
+}
