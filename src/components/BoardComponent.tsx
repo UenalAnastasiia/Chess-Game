338203633c8765caecf0f85b1af922ca6e460b82
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-08-06 16:50:19 +0200

    move figure to cell

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Board } from '../models/Board';
 import CellComponent from './CellComponent';
 import { Cell } from '../models/Cell';
@@ -13,7 +13,29 @@ const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
 
 
     function click(cell: Cell) {
-        setSelectedCell(cell);
+        if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+            selectedCell.moveFigure(cell);
+            setSelectedCell(null);
+        } else {
+            setSelectedCell(cell);
+        }
+    }
+
+
+    useEffect(() => {
+        highlightCells();
+    }, [selectedCell]);
+
+
+    function highlightCells() {
+       board.highlightCells(selectedCell); 
+       updateBoard();
+    }
+
+
+    function updateBoard() {
+        const newBoard = board.getCopyBoard();
+        setBoard(newBoard);
     }
 
     return (
